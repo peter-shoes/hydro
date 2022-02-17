bff06ae231e145fe55bf1178704717afbab44d3a
Peter Shoes <bruce.mcculloch64@gmail.com>
2022-02-17 00:31:06 -0500

    possible final

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <headers.hpp>
 
+int col_time = 0;
+
 void setup() {
   Serial.begin(9600);
   AFMS.begin();
@@ -11,16 +13,18 @@ void loop() {
   check_pump(pump1);
   check_pump(pump2);
   check_pump(pump3);
-  dt_chk = dt_chk + TimeSpan(0,0,0,1);
+  
+  dt_chk = dt_chk + TimeSpan(0,0,0,col_time+1);
   delay(1000);
+  col_time = 0;
+
 }
 
 void check_pump(Pump pmp) {
   if ((dt_chk == dt_init) || ((dt_chk - pmp.last_dose_time).seconds() >= pmp.hrs_btwn_dose*3600)){
     run_mot(pmp);
-    Serial.println("RUNNING");
   }
-  Serial.println((dt_chk - pmp.last_dose_time).seconds());
+  // Serial.println((dt_chk - pmp.last_dose_time).seconds());
 }
 
 void run_mot(Pump pmp) {
@@ -28,6 +32,6 @@ void run_mot(Pump pmp) {
   pmp.mot->run(FORWARD);
   delay(pmp.runtime * 1000);
   pmp.mot->fullOff();
-  dt_chk = dt_chk + TimeSpan(0,0,0,pmp.runtime);
+  col_time += pmp.runtime;
   pmp.last_dose_time = dt_chk;
 }
